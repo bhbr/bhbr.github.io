07328bfb6d301f6a8081c54812d2935ddaaa5586
Ben Hambrecht <bhbr@users.noreply.github.com>
2025-05-06 21:56:00 +0200

    removed testing

--- minter/lib/startPaper.js
+++ minter/lib/startPaper.js
@@ -1,12 +1,7 @@
 import { CoinFlipPaper } from './extensions/boards/coin-flip/CoinFlipPaper.js';
-import { AllTests } from './_tests/allTests.js';
 export class StartPaper extends CoinFlipPaper {
     defaults() { return {}; }
     mutabilities() { return {}; }
-}
-let TESTING = true;
-if (TESTING) {
-    AllTests.run();
 }
 export const paper = new StartPaper();
 //# sourceMappingURL=startPaper.js.map
