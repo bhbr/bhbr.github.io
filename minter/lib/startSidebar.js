0667f1fb71882e7b92dc8a765d8c5e25c0d2e4e3
Ben Hambrecht <bhbr@users.noreply.github.com>
2025-05-06 21:52:46 +0200

    filenames are now case-sensitive

---
+++ minter/lib/startSidebar.js
@@ -0,0 +1,7 @@
+import { CoinFlipSidebar } from './extensions/boards/coin-flip/CoinFlipSidebar.js';
+export class StartSidebar extends CoinFlipSidebar {
+    defaults() { return {}; }
+    mutabilities() { return {}; }
+}
+export const sidebar = new StartSidebar();
+//# sourceMappingURL=StartSidebar.js.map
