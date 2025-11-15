d6562a4bcaeb8416959a2836d685f780781eb6bd
Ben Hambrecht <bhbr@users.noreply.github.com>
2025-11-15 22:47:50 +0100

    ok NOW testing is disabled

--- minter/src/StartPaper.ts
+++ minter/src/StartPaper.ts
@@ -2,13 +2,13 @@
 import { DemoPaper } from './extensions/boards/demo/DemoPaper'
 import { CoinFlipPaper } from './extensions/boards/coin-flip/CoinFlipPaper'
 import { log } from './core/functions/logging'
-import { AllTests } from './_tests/allTests'
+//import { AllTests } from './_tests/allTests'
 
 
 export class StartPaper extends CoinFlipPaper { }
 
 export const TESTING = false
 
-if (TESTING) { AllTests.run() }
+//if (TESTING) { AllTests.run() }
 
 export const paper = new StartPaper()
