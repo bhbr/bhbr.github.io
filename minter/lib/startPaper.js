bf9f653055c681198863ad657ce65c95c720517e
Ben Hambrecht <bhbr@users.noreply.github.com>
2025-11-17 01:10:47 +0100

    disabling tests

--- minter/lib/startPaper.js
+++ minter/lib/startPaper.js
@@ -1,12 +1,12 @@
 import { CoinFlipPaper } from './extensions/boards/coin-flip/CoinFlipPaper.js';
-import { AllTests } from './_tests/allTests.js';
+//import { AllTests } from './_tests/allTests.js';
 export class StartPaper extends CoinFlipPaper {
     defaults() { return {}; }
     mutabilities() { return {}; }
 }
 export const TESTING = true;
 if (TESTING) {
-    AllTests.run();
+//    AllTests.run();
 }
 export const paper = new StartPaper();
 //# sourceMappingURL=startPaper.js.map
