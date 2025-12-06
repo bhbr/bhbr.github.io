10266804f92429d86cccafa29563bba234331306
Ben Hambrecht <bhbr@users.noreply.github.com>
2025-12-06 15:26:42 +0100

    try again

--- minter/lib/startPaper.js
+++ minter/lib/startPaper.js
@@ -3,6 +3,7 @@ export class StartPaper extends CoinFlipPaper {
     defaults() { return {}; }
     mutabilities() { return {}; }
 }
-
+// export const TESTING = true
+// if (TESTING) { AllTests.run() }
 export const paper = new StartPaper();
 //# sourceMappingURL=startPaper.js.map
