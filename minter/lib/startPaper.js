73c6d7169e6107fa0c2edd243eaf4830275bae0e
Ben Hambrecht <bhbr@users.noreply.github.com>
2026-05-05 04:10:15 +0200

    update

--- minter/lib/startPaper.js
+++ minter/lib/startPaper.js
@@ -1,15 +1,9 @@
 //import { AllTests } from './_tests/allTests'
 import { CoinFlipPaper } from './extensions/boards/coin-flip/CoinFlipPaper.js';
-import { NumberListBox } from './extensions/creations/math/boxes/NumberListBox.js';
 export class StartPaper extends CoinFlipPaper {
     defaults() { return {}; }
     mutabilities() { return {}; }
 }
 //AllTests.run()
 export const paper = new StartPaper();
-let box = new NumberListBox({
-    value: [0, 1, 0],
-    anchor: [100, 100]
-});
-paper.addToContent(box);
 //# sourceMappingURL=startPaper.js.map
